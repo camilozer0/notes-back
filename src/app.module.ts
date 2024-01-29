6dd9cff49a6cf035acb7f4df378ac4d0c03d7e7f
Camilo Mazorra <mazorra006@gmail.com>
2024-01-29 12:01:36 -0500

    configurando entidad y conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ToDoModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,     
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true
-    })
-    ],
+    }),
+    ToDoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/to-do/entities/to-do.entity.ts
+++ src/to-do/entities/to-do.entity.ts
@@ -1,18 +1,31 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { arrayBuffer } from "stream/consumers";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
+@Entity({ name: 'toDo'})
 export class ToDo {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column('text')
     title: string;
 
+    @Column('text')
     description: string;
 
-    dateOfCreation: Date;
+    @Column('date')
+    createdAt: Date;
 
+    @Column('date')
     dueDate: Date;
 
+    @Column('bool', {
+        default: true
+    })
+    isActive: boolean
+
+    @Column('text', {
+        array: true
+    })
     label: string[];
 }
