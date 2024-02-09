0bf855f4e18938c98f1af3c3407695537dfcb355
Camilo Mazorra <mazorra006@gmail.com>
2024-02-09 12:03:19 -0500

    editando auth 1.0

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { SignupUserDto } from './dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
 
-  signup(signupUserDto: SignupUserDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+
+  async signup(signupUserDto: SignupUserDto) {
+    try {
+      const { password, ...userDto } = signupUserDto;
+      const hashPw = await hash(password, 9);
+      const userToSave = {hashPw, ...userDto };
+      this.userRepository.create( userToSave );
+      this.userRepository.save( userToSave )
+      return userToSave;
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
 
   login(loginUserDto: LoginUserDto) {
